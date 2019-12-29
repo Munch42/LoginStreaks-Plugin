a76927cf1cfe295ebdf953a70c9e0abc71e04d7a
Munch42 <Munch4242@gmail.com>
2019-12-29 16:04:55 -0700

    Update 2.1.1

--- src/main/java/me/munch42/loginstreak/Main.java
+++ src/main/java/me/munch42/loginstreak/Main.java
@@ -8,7 +8,6 @@ import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
